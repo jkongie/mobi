79704c2890fb424cf37fc0b32d3882e3af625133
James Kong <jkongie@gmail.com>
2012-04-12 18:30:19 +0800

    Clean up the stream slicer

--- lib/mobi/stream_slicer.rb
+++ lib/mobi/stream_slicer.rb
@@ -1,38 +1,41 @@
 module Mobi
   class StreamSlicer
 
-    attr_reader :stream, :length
+    attr_reader   :stream, :length
     attr_accessor :start, :stop
 
-    def initialize(stream, start=0, stop=nil)
+    def initialize(stream, start = 0, stop = nil)
       @stream = stream
       @start  = start
+
       if stop.nil?
         stream.seek(0, 2)
         stop = stream.tell
       end
-      @stop = stop
+
+      @stop    = stop
       @length  = stop - start
     end
 
     def [](offset, bytes=1)
       stream = @stream
-      base = @start
-      
+      base   = @start
+
       if bytes == 1
         stream.seek(base + offset)
         return stream.read(1)
       end
-      
+
       start = offset
       stop  = offset + bytes
-      
+
       # Reverse if you want to pass in negative bytes
       start, stop = stop, start if bytes < 0
-      
+
+      # I can't find a use case where it will ever get here
       size = stop - start
-      return "" if size <= 0
-      
+      return nil if size <= 0
+
       stream.seek(base + start)
       data = stream.read(size)
       return data
--- spec/lib/mobi/stream_slicer_spec.rb
+++ spec/lib/mobi/stream_slicer_spec.rb
@@ -1,64 +1,62 @@
 require 'spec_helper'
 
 describe Mobi::StreamSlicer do
-  let(:file){ File.open('spec/fixtures/test.mobi')}
-  
+  let(:file){ File.open('spec/fixtures/test.mobi') }
+
   context "instantiation" do
     it "should set the start point to 0 if no start is provided" do
       ss = Mobi::StreamSlicer.new(file)
-      ss.start.should == 0     
+      ss.start.should == 0
     end
-    
+
     it "should set the end point to the file end if no stop is provided" do
       ss = Mobi::StreamSlicer.new(file)
       ss.stop.should == 63697
     end
-    
+
     it "should set the start and stop points to the arguments provided" do
       ss = Mobi::StreamSlicer.new(file, 1, 2)
       ss.start.should == 1
       ss.stop.should == 2
     end
-    
+
     it "should set the length" do
       ss = Mobi::StreamSlicer.new(file, 1, 10)
       ss.length.should == 9
     end
-    
+
     it "should set the stream to the input file" do
       ss = Mobi::StreamSlicer.new(file)
       ss.stream.should == file
     end
   end
-  
-  context "retrieval" do
-    
+
+  context "#[]" do
+
     it "should return 1 byte" do
       ss = Mobi::StreamSlicer.new(file)
       ss[0].length.should == 1
     end
-    
+
     it "should start the search from the offset provided" do
       file.seek(5)
       value = file.read(1)
       ss = Mobi::StreamSlicer.new(file, 5)
       ss[0].should == value
     end
-    
+
     it "should return the correct number of bytes" do
       file.seek(5)
       value = file.read(2)
       ss = Mobi::StreamSlicer.new(file, 5)
       ss[0, 2].should == value
     end
-    
+
     it "should seek in reverse order" do
       file.seek(8)
       value = file.read(2)
       ss = Mobi::StreamSlicer.new(file, 5)
       ss[5, -2].should == value
     end
-    
-    it "should return an empty string if the size is less than or equal to 0"
   end
 end
