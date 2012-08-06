1fd6842548b7e9471354fe33526416a983fb328d
James Kong <jkongie@gmail.com>
2012-08-06 19:50:53 +0800

    Fix mobi spec by require the mobi file

--- spec/lib/mobi_spec.rb
+++ spec/lib/mobi_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
+require 'mobi'
+
 describe Mobi do
   it "should instantiate a Mobi::Metadata object" do
-    file = File.open('spec/fixtures/test.mobi')
+    file = File.open('spec/fixtures/sherlock.mobi')
     Mobi.metadata(file).should be_an_instance_of(Mobi::Metadata)
   end
 end
